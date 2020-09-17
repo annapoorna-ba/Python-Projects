ee2830cfec35fa862ca0d79ea0cfac5960341278
Annapoorna_BA <43178346+annapoorna-ba@users.noreply.github.com>
2020-09-17 22:29:22 +0530

    Update Calculator.py

--- Simple Calculator/Calculator.py
+++ Simple Calculator/Calculator.py
@@ -1,45 +1,64 @@
 import tkinter as tk
 
 root=tk.Tk()
-root.geometry("390x250")
-root.configure(bg='slate gray1')
+root.geometry("500x540")
+root.configure(bg='gray75')
 root.title("Calculator")
-eq=''
+#slate gray1
+
 def write(x):
     if x=='':
-        txt.delete("1.0",tk.END)
+        txt.delete("1.0",tk.END)  
     else:
+        if "=" in txt.get("1.0", "end-1c"):
+            txt.delete("1.0",tk.END)       
         txt.insert(tk.END,x)
-      
+   
+def delete():
+    if "="  not in txt.get("1.0", "end-1c"):
+        txt.delete("end-2c",tk.END)
+       
+    
 def res():
     try:
         ans=eval(txt.get("1.0", "end-1c"))
-        txt.delete("1.0",tk.END)
-        txt.insert(tk.END,str(ans))
+        #txt.delete("1.0",tk.END)
+        txt.insert(tk.END,' = '+str(ans))
     except:
         txt.delete("1.0",tk.END)
         txt.insert(tk.END,"Error")
     
-txt=tk.Text(root,height=1,width=32,font=("Helvetica",15))
-txt.place(x=15,y=10)
-fg='tomato'
-fg_sign='khaki1'
-b_bg='lemon chiffon'
-tk.Button(root,text='1',width=10,height=2,activebackground=fg,command=lambda:write(1),bg=b_bg).place(x=20,y=50)
-tk.Button(root,text='2',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(2)).place(x=115,y=50)
-tk.Button(root,text='3',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(3)).place(x=205,y=50)
-tk.Button(root,text='+',width=10,height=2,activebackground=fg_sign,bg=b_bg,command=lambda:write('+')).place(x=295,y=50)
-tk.Button(root,text='4',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(4)).place(x=20,y=100)
-tk.Button(root,text='5',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(5)).place(x=115,y=100)
-tk.Button(root,text='6',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(6)).place(x=205,y=100)
-tk.Button(root,text='-',width=10,height=2,activebackground=fg_sign,bg=b_bg,command=lambda:write('-')).place(x=295,y=100)
-tk.Button(root,text='7',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(7)).place(x=20,y=150)
-tk.Button(root,text='8',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(8)).place(x=115,y=150)
-tk.Button(root,text='9',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(9)).place(x=205,y=150)
-tk.Button(root,text='*',width=10,height=2,activebackground=fg_sign,bg=b_bg,command=lambda:write('*')).place(x=295,y=150)
-tk.Button(root,text='0',width=10,height=2,activebackground=fg,bg=b_bg,command=lambda:write(0)).place(x=115,y=200)
-tk.Button(root,text='clear',width=10,height=2,activebackground='black',activeforeground='white',bg=b_bg,command=lambda:write("")).place(x=20,y=200)
-tk.Button(root,text='=',width=10,height=2,activebackground='medium sea green',bg=b_bg,command=res).place(x=205,y=200)
-tk.Button(root,text='/',width=10,height=2,activebackground=fg_sign,bg=b_bg,command=lambda:write('/')).place(x=295,y=200)
+
+txt=tk.Text(root,height=2,width=29,font=("Helvetica",20,'bold'),borderwidth=10,bg="slate gray1")
+txt.place(x=20,y=30)
+#fg='tomato'
+fg="gold4"
+fg_sign='khaki'
+b_bg='khaki3'
+
+x=14
+y=180
+
+tk.Button(root,text='1',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(1)).place(x=x,y=y)
+tk.Button(root,text='2',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(2)).place(x=x+120,y=y)
+tk.Button(root,text='3',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(3)).place(x=x+240,y=y)
+tk.Button(root,text='+',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg_sign,bg=b_bg,command=lambda:write('+')).place(x=x+360,y=y)
+tk.Button(root,text='4',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(4)).place(x=x,y=y+70)
+tk.Button(root,text='5',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(5)).place(x=x+120,y=y+70)
+tk.Button(root,text='6',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(6)).place(x=x+240,y=y+70)
+tk.Button(root,text='-',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg_sign,bg=b_bg,command=lambda:write('-')).place(x=x+360,y=y+70)
+tk.Button(root,text='7',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(7)).place(x=x,y=y+140)
+tk.Button(root,text='8',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(8)).place(x=x+120,y=y+140)
+tk.Button(root,text='9',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(9)).place(x=x+240,y=y+140)
+tk.Button(root,text='x',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg_sign,bg=b_bg,command=lambda:write('*')).place(x=x+360,y=y+140)
+tk.Button(root,text='0',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(0)).place(x=x+120,y=y+210)
+tk.Button(root,text='clear',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground='black',activeforeground='white',bg=b_bg,command=lambda:write("")).place(x=x+120,y=y+280)
+tk.Button(root,text='=',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground='medium sea green',bg=b_bg,command=res).place(x=x+360,y=y+280)
+tk.Button(root,text='/',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg_sign,bg=b_bg,command=lambda:write('/')).place(x=x+360,y=y+210)
+tk.Button(root,text='(',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,command=lambda:write('('),bg=b_bg).place(x=x,y=y+280)
+tk.Button(root,text='.',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write('.')).place(x=x+240,y=y+210)
+tk.Button(root,text=')',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg,bg=b_bg,command=lambda:write(')')).place(x=x+240,y=y+280)
+tk.Button(root,text='mod',width=9,height=2,font=("Helvetica",13,'bold'),borderwidth=8,activebackground=fg_sign,bg=b_bg,command=lambda:write('%')).place(x=x,y=y+210)
+tk.Button(root,text='back',width=7,height=1,font=("Helvetica",10,'bold'),borderwidth=8,activebackground='tomato',bg=b_bg,command=delete).place(x=x,y=130)
 tk.mainloop()
 
